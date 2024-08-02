e92403cf8ac465051c174ff489e484eda5672e0b
Dimitri <dimitri3991@gmail.com>
2024-08-01 23:49:27 -0700

    Adjusting web portal to gcp host, adding image drawing logic

--- API/app/api.py
+++ API/app/api.py
@@ -3,12 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from PIL import Image
 import io
+import torch
+from ultralytics import YOLO
+import cv2
+import numpy as np
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
-    "localhost:3000"
+    "https://ui-service-dot-eloquent-env-430802-s8.uw.r.appspot.com"
 ]
 
 
@@ -21,6 +24,9 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+model = YOLO("/Users/dimitridumont/code/skool/501/AAI-501/detect/train8/weights/best.pt")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
@@ -34,17 +40,34 @@ async def upload_file(file: UploadFile = File(...)):
     
     try:
         # Read the image file
-        image = Image.open(file.file)
-        
-        # Example processing: Convert image to grayscale
-        grayscale_image = image.convert('L')
-        
-        # Save processed image to a BytesIO object
-        img_io = io.BytesIO()
-        grayscale_image.save(img_io, 'JPEG')
-        img_io.seek(0)
-        
-        return StreamingResponse(img_io, media_type="image/jpeg")
+        image = Image.open(io.BytesIO(await file.read())).convert("RGB")
+        image_np = np.array(image)
+
+        # Run the image through the YOLO model
+        results = model(image_np)
+
+        # Process the model output to draw bounding boxes
+        for result in results:
+            boxes = result.boxes.xyxy.numpy()  # Bounding boxes in (x1, y1, x2, y2) format
+            scores = result.boxes.conf.numpy()  # Confidence scores
+            labels = result.boxes.cls.numpy()  # Class labels
+
+            for box, score, label in zip(boxes, scores, labels):
+                if score > 0.5:  # Only consider detections with a confidence score above 0.5
+                    has_detections = True
+                    x1, y1, x2, y2 = map(int, box)
+                    cv2.rectangle(image_np, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.putText(image_np, f"{int(label)}: {score:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+        # if not has_detections:
+        # # Draw "No Tumor" text on the image
+        #     cv2.putText(image_np, "No Tumor", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        # Convert the image back to a byte stream
+        is_success, buffer = cv2.imencode(".jpg", image_np)
+        io_buf = io.BytesIO(buffer)
+
+        return StreamingResponse(io_buf, media_type="image/jpeg")
     
     except Exception as e:
         raise HTTPException(status_code=500, detail="Processing error")
