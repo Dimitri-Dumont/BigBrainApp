6605363733ca4376f8d96f193419608e816c7056
Dimitri <dimitri3991@gmail.com>
2024-08-02 17:37:18 -0700

    Tweaking api folder for google clou deplyoy

--- API/main.py
+++ API/main.py
@@ -1,5 +1,6 @@
 import uvicorn
-
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8080))  # Ensure to use the PORT environment variable
+    uvicorn.run("api:app", host="0.0.0.0", port=port)
