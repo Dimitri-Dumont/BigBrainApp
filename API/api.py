3fb74fe532a7a6ba487be9a6d41ca0f12eabd214
Dimitri <dimitri3991@gmail.com>
2024-08-02 18:36:41 -0700

    More web tweaks

--- API/api.py
+++ API/api.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse
 from PIL import Image
-import io
-import torch
 from ultralytics import YOLO
-import cv2
 import numpy as np
+import torch
+import cv2
+import io
+from starlette.responses import StreamingResponse
+import traceback
 
 app = FastAPI()
 
 origins = [
-    "https://ui-service-dot-eloquent-env-430802-s8.uw.r.appspot.com"
+    "https://ui-service-dot-eloquent-env-430802-s8.uw.r.appspot.com",  # Your production URL
+    "http://localhost:3000"  # Local development URL
 ]
-
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,14 +24,13 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-model = YOLO("best.pt")
+model = YOLO("./best.pt")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to our kickass application boi."}
-
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
@@ -45,6 +44,7 @@ async def upload_file(file: UploadFile = File(...)):
 
         # Run the image through the YOLO model
         results = model(image_np)
+        has_detections = False  # Initialize the variable here
 
         # Process the model output to draw bounding boxes
         for result in results:
@@ -59,15 +59,21 @@ async def upload_file(file: UploadFile = File(...)):
                     cv2.rectangle(image_np, (x1, y1), (x2, y2), (0, 255, 0), 2)
                     cv2.putText(image_np, f"{int(label)}: {score:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
 
-        # if not has_detections:
-        # # Draw "No Tumor" text on the image
-        #     cv2.putText(image_np, "No Tumor", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+        
+            if not has_detections:
+            # Draw "No Tumor" text on the image
+                cv2.putText(image_np, "No Tumor", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
 
         # Convert the image back to a byte stream
         is_success, buffer = cv2.imencode(".jpg", image_np)
         io_buf = io.BytesIO(buffer)
+        if(not is_success):
+            print("wtf")
+            traceback.print_exc()
 
         return StreamingResponse(io_buf, media_type="image/jpeg")
     
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Processing error")
+        # Log the exception for debugging
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=f"Processing error: {str(e)}")
